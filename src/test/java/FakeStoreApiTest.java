7019dafc76e9854692e67e98870def107bcff51d
ebejara <enrique@bejaranomail.com>
2026-01-01 17:49:19 +0100

    Test test

--- src/test/java/FakeStoreApiTest.java
+++ src/test/java/FakeStoreApiTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class FakeStoreApiTest {
+public class FakeStoreApiTest {  // MÅSTE vara public!
     private static final Logger logger = LoggerFactory.getLogger(FakeStoreApiTest.class);
 
     @Test
@@ -23,12 +23,8 @@ public class FakeStoreApiTest {
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        // Kontrollera statuskod
         assertEquals(200, response.statusCode());
-
-        // Extra kontroller
         assertTrue(response.body().length() > 0);
-        assertEquals("application/json; charset=utf-8", response.headers().firstValue("content-type").orElse(""));
 
         logger.info("API test completed with status: {}", response.statusCode());
     }
